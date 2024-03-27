6f6cb59ff9cac8c7659c91fa3ca3f427fa752bf6
juhonamnam <jhnamnam@gmail.com>
2024-03-28 01:19:42 +0900

    work in progress

--- src/component/gallery/index.tsx
+++ src/component/gallery/index.tsx
@@ -1,39 +1,45 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import image1 from "../../image/sample1.png"
 import image2 from "../../image/sample2.png"
 import image3 from "../../image/sample3.png"
 
 const ITEMS = [
-  <img src={image1} draggable={false} />,
-  <img src={image2} draggable={false} />,
-  <img src={image3} draggable={false} />,
-]
-
-const ITEMS_WITH_KEYS = ITEMS.map((item, idx) => ({ key: idx, item }))
+  <img src={image1} draggable={false} alt="image1" />,
+  <img src={image2} draggable={false} alt="image2" />,
+  <img src={image3} draggable={false} alt="image3" />,
+].map((item, idx) => (
+  <div className="carousel-item" key={idx}>
+    {item}
+  </div>
+))
 
 export const Gallery = () => {
   const carouselRef = useRef<HTMLDivElement>(
     null,
   ) as React.MutableRefObject<HTMLDivElement>
-  const [slide, setSlide] = useState(1)
-  const [isDragging, setIsDragging] = useState(false)
+  const [slide, setSlide] = useState(0)
+  const [status, setStatus] = useState<
+    "stationary" | "dragging" | "dragEnding" | "prepareMoving" | "moving"
+  >("stationary")
   const [dragOption, setDragOption] = useState({
     startingClientX: 0,
     currentTranslateX: 0,
   })
-
-  const [isTransitioning, setIsTransitioning] = useState(false)
+  const [moveOption, setMoveOption] = useState({
+    currentTranslateX: 0,
+    srcIdx: 0,
+    destIdx: 0,
+  })
 
   const dragStart = (clientX: number) => {
-    if (isTransitioning || isDragging) return
+    if (status !== "stationary") return
     setDragOption({
       startingClientX: clientX,
       currentTranslateX: -carouselRef.current.clientWidth,
     })
-    setIsDragging(true)
+    setStatus("dragging")
   }
 
-  // drag 중 이벤트
   const dragging = useCallback(
     (clientX: number) => {
       let moveTranslateX = clientX - dragOption.startingClientX
@@ -52,9 +58,28 @@ export const Gallery = () => {
     [dragOption],
   )
 
-  // drag 종료 이벤트
+  const onMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (status === "dragging") {
+        e.preventDefault()
+        dragging(e.clientX)
+      }
+    },
+    [status, dragging],
+  )
+
+  const onTouchMove = useCallback(
+    (e: TouchEvent) => {
+      if (status === "dragging") {
+        e.preventDefault()
+        dragging(e.targetTouches[0].clientX)
+      }
+    },
+    [status, dragging],
+  )
+
   const dragEnd = useCallback(() => {
-    if (isDragging) {
+    if (status === "dragging") {
       let move = 0
       if (
         dragOption.currentTranslateX <
@@ -73,35 +98,18 @@ export const Gallery = () => {
         currentTranslateX: -carouselRef.current.clientWidth * (move + 1),
       }))
 
-      setIsTransitioning(true)
+      setStatus("dragEnding")
 
       setTimeout(() => {
         setDragOption((prev) => ({
           ...prev,
           currentTranslateX: -carouselRef.current.clientWidth,
         }))
-        setIsTransitioning(false)
-        setIsDragging(false)
+        setStatus("stationary")
         setSlide((prev) => (prev + move + ITEMS.length) % ITEMS.length)
       }, 300)
     }
-  }, [dragOption, isDragging])
-
-  const onMouseMove = useCallback(
-    (e: MouseEvent) => {
-      if (!isDragging || isTransitioning) return
-      dragging(e.clientX)
-    },
-    [isDragging, isTransitioning, dragging],
-  )
-
-  const onTouchMove = useCallback(
-    (e: TouchEvent) => {
-      if (!isDragging || isTransitioning) return
-      dragging(e.targetTouches[0].clientX)
-    },
-    [isDragging, isTransitioning, dragging],
-  )
+  }, [dragOption, status])
 
   useEffect(() => {
     window.addEventListener("mousemove", onMouseMove)
@@ -117,6 +125,56 @@ export const Gallery = () => {
     }
   }, [onMouseMove, onTouchMove, dragEnd])
 
+  const moveTo = useCallback(
+    (idx: number) => {
+      if (status === "stationary") {
+        setSlide(idx)
+        setStatus("prepareMoving")
+        let srcTranslateX
+        let dstTranslateX: number
+        if (slide < idx) {
+          srcTranslateX = 0
+          dstTranslateX = -carouselRef.current.clientWidth * (idx - slide)
+        } else {
+          srcTranslateX = -carouselRef.current.clientWidth * (slide - idx)
+          dstTranslateX = 0
+        }
+
+        setMoveOption({
+          currentTranslateX: srcTranslateX,
+          srcIdx: slide,
+          destIdx: idx,
+        })
+
+        setTimeout(() => {
+          setStatus("moving")
+          setMoveOption({
+            currentTranslateX: dstTranslateX,
+            srcIdx: slide,
+            destIdx: idx,
+          })
+          setTimeout(() => {
+            setStatus("stationary")
+          }, 300)
+        }, 0)
+      }
+    },
+    [status, slide],
+  )
+
+  const transformStyle = useMemo(() => {
+    switch (status) {
+      case "dragging":
+      case "dragEnding":
+        return { transform: `translateX(${dragOption.currentTranslateX}px)` }
+      case "prepareMoving":
+      case "moving":
+        return { transform: `translateX(${moveOption.currentTranslateX}px)` }
+      default:
+        return {}
+    }
+  }, [status, dragOption, moveOption])
+
   return (
     <div className="section gallery">
       <h1>겔러리</h1>
@@ -128,38 +186,33 @@ export const Gallery = () => {
           onTouchStart={(e) => dragStart(e.targetTouches[0].clientX)}
         >
           <div
-            className={`carousel-list${isTransitioning ? " transitioning" : ""}`}
-            style={
-              isDragging
-                ? { transform: `translateX(${dragOption.currentTranslateX}px)` }
-                : {}
-            }
+            className={`carousel-list${["dragEnding", "moving"].includes(status) ? " transitioning" : ""}`}
+            style={transformStyle}
           >
-            {isDragging ? (
-              <>
-                {[
-                  ...(slide === 0
-                    ? [ITEMS_WITH_KEYS[ITEMS_WITH_KEYS.length - 1]]
-                    : []),
-                  ...ITEMS_WITH_KEYS.slice(
-                    slide === 0 ? 0 : slide - 1,
-                    slide + 2,
-                  ),
-                  ...(slide === ITEMS_WITH_KEYS.length - 1
-                    ? [ITEMS_WITH_KEYS[0]]
-                    : []),
-                ].map(({ item, key }) => (
-                  <div className="carousel-item" key={key}>
-                    <h2>{item}</h2>
-                  </div>
-                ))}
-              </>
-            ) : (
-              <div className="carousel-item">
-                <h2>{ITEMS[slide]}</h2>
-              </div>
-            )}
+            {["dragging", "dragEnding"].includes(status) && [
+              ...(slide === 0 ? [ITEMS[ITEMS.length - 1]] : []),
+              ...ITEMS.slice(slide === 0 ? 0 : slide - 1, slide + 2),
+              ...(slide === ITEMS.length - 1 ? [ITEMS[0]] : []),
+            ]}
+            {["prepareMoving", "moving"].includes(status) &&
+              ITEMS.slice(
+                Math.min(moveOption.srcIdx, moveOption.destIdx),
+                Math.max(moveOption.srcIdx, moveOption.destIdx) + 1,
+              )}
+            {status === "stationary" && ITEMS[slide]}
           </div>
+        </div>
+        <div className="carousel-indicator">
+          {ITEMS.map((_, idx) => (
+            <div
+              key={idx}
+              className={`indicator${idx === slide ? " active" : ""}`}
+              onClick={() => {
+                if (idx === slide) return
+                moveTo(idx)
+              }}
+            />
+          ))}
         </div>
       </div>
     </div>
