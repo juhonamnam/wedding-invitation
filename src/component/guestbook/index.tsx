93e78db7e59331390e53a1accd43adfc132da83a
juhonamnam <jhnamnam@gmail.com>
2024-05-18 03:14:13 +0900

    all guestbook modal

--- src/component/guestbook/index.tsx
+++ src/component/guestbook/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { Button } from "../button"
 import { dayjs } from "../../const"
 import { LazyDiv } from "../lazyDiv"
@@ -17,7 +17,16 @@ const RULES = {
   },
 }
 
-const DUMMY_GUEST_BOOK = [
+const PAGES_PER_BLOCK = 5
+
+type Post = {
+  id: number
+  timestamp: number
+  name: string
+  content: string
+}
+
+const DUMMY_POSTS = [
   {
     id: 0,
     timestamp: 1713795179,
@@ -29,28 +38,34 @@ const DUMMY_GUEST_BOOK = [
 export const GuestBook = () => {
   const { openModal } = useModal()
 
-  const [guestBook, setGuestBook] =
-    useState<
-      { id: number; timestamp: number; name: string; content: string }[]
-    >(DUMMY_GUEST_BOOK)
+  const [posts, setPosts] = useState<Post[]>([])
+
+  // TODO: Implement loadPosts
+  const loadPosts = () => {
+    setPosts(DUMMY_POSTS)
+  }
+
+  useEffect(() => {
+    loadPosts()
+  }, [])
 
   return (
     <LazyDiv className="card guestbook">
       <h2 className="english">Guest Book</h2>
 
-      {guestBook.map((guest) => (
-        <div key={guest.id} className="post">
+      {posts.map((post) => (
+        <div key={post.id} className="post">
           <div className="heading">
             <div className="close-button" />
           </div>
           <div className="body">
             <div className="title">
-              <div className="name">{guest.name}</div>
+              <div className="name">{post.name}</div>
               <div className="date">
-                {dayjs.unix(guest.timestamp).format("YYYY-MM-DD")}
+                {dayjs.unix(post.timestamp).format("YYYY-MM-DD")}
               </div>
             </div>
-            <div className="content">{guest.content}</div>
+            <div className="content">{post.content}</div>
           </div>
         </div>
       ))}
@@ -58,21 +73,21 @@ export const GuestBook = () => {
       <div className="break" />
 
       <Button
-        onClick={() => {
-          openModal(<WriteGuestBookModal />)
-        }}
+        onClick={() => openModal(<WriteGuestBookModal loadPosts={loadPosts} />)}
       >
         방명록 작성하기
       </Button>
 
       <div className="break" />
 
-      <Button>방명록 전체보기</Button>
+      <Button onClick={() => openModal(<AllGuestBookModal />)}>
+        방명록 전체보기
+      </Button>
     </LazyDiv>
   )
 }
 
-const WriteGuestBookModal = () => {
+const WriteGuestBookModal = ({ loadPosts }: { loadPosts: () => void }) => {
   const inputRef = useRef({}) as React.MutableRefObject<{
     name: HTMLInputElement
     content: HTMLTextAreaElement
@@ -129,7 +144,10 @@ const WriteGuestBookModal = () => {
               return
             }
 
+            // TODO: Implement writeGuestBook
+
             closeModal()
+            loadPosts()
           }}
         >
           이름
@@ -159,6 +177,93 @@ const WriteGuestBookModal = () => {
           />
           <Button type="submit">저장하기</Button>
         </form>
+      </div>
+    </div>
+  )
+}
+
+const AllGuestBookModal = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [currentPage, setCurrentPage] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
+
+  // TODO: Implement loadPage
+  const loadPage = (page: number) => {
+    setCurrentPage(page)
+
+    setPosts(DUMMY_POSTS)
+    setTotalPages(1)
+  }
+
+  useEffect(() => {
+    loadPage(0)
+  }, [])
+
+  const pages = useMemo(() => {
+    const start = Math.floor(currentPage / PAGES_PER_BLOCK) * PAGES_PER_BLOCK
+    const end = Math.min(start + PAGES_PER_BLOCK, totalPages)
+
+    return Array.from({ length: end - start }).map((_, index) => index + start)
+  }, [currentPage, totalPages])
+
+  return (
+    <div className="all-guestbook-modal">
+      <div className="content">
+        <div className="title">방명록 전체보기</div>
+
+        {posts.map((post) => (
+          <div key={post.id} className="post">
+            <div className="heading">
+              <div className="close-button" />
+            </div>
+            <div className="body">
+              <div className="title">
+                <div className="name">{post.name}</div>
+                <div className="date">
+                  {dayjs.unix(post.timestamp).format("YYYY-MM-DD")}
+                </div>
+              </div>
+              <div className="content">{post.content}</div>
+            </div>
+          </div>
+        ))}
+
+        <div className="break" />
+
+        <div className="pagination">
+          {pages[0] > 0 && (
+            <div
+              className="page"
+              onClick={() => {
+                loadPage(pages[0] - 1)
+              }}
+            >
+              이전
+            </div>
+          )}
+          {pages.map((page) => (
+            <div
+              className={`page${page === currentPage ? " current" : ""}`}
+              key={page}
+              onClick={() => {
+                if (page === currentPage) return
+                loadPage(page)
+              }}
+            >
+              {page + 1}
+            </div>
+          ))}
+          {pages[pages.length - 1] < totalPages - 1 && (
+            <div
+              className="page"
+              onClick={() => {
+                loadPage(pages[pages.length - 1] + 1)
+              }}
+            >
+              다음
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
