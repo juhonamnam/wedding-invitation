50e965f446097ee021d49acb821111291ce7236b
juhonamnam <jhnamnam@gmail.com>
2024-06-05 21:48:07 +0900

    new design

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -2,28 +2,42 @@ import { BRIDE, GROOM, WEDDING_DATE } from "../../const"
 import coverImage from "../../image/cover.jpg"
 import { LazyDiv } from "../lazyDiv"
 
+const DAY_OF_WEEK = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
+
 export const Cover = () => {
   return (
     <LazyDiv className="card cover">
-      <div className="title-group">
-        <div className="title">우리</div>
-        <div className="title-deco">
-          <div>{WEDDING_DATE.format("MM")}</div>
-          <div>{WEDDING_DATE.format("DD")}</div>
-        </div>
-        <div className="title">결혼합니다</div>
+      <div className="wedding-date">
+        {WEDDING_DATE.format("YYYY")}
+        <div className="divider" />
+        {WEDDING_DATE.format("MM")}
+        <div className="divider" />
+        {WEDDING_DATE.format("DD")}
+      </div>
+      <div className="wedding-day-of-week">
+        {DAY_OF_WEEK[WEDDING_DATE.day()]}
       </div>
       <div className="image-wrapper">
         <img src={coverImage} alt="sample" />
       </div>
       <div className="subtitle">Save the date for the wedding of</div>
-      <div className="content">
-        {GROOM} & {BRIDE}
+      <div className="names">
+        {GROOM}
+        <div className="divider" />
+        {BRIDE}
       </div>
-      <div className="content">
+      <div className="info">
         {WEDDING_DATE.format("YYYY년 MMMM D일 dddd A h시")}
       </div>
-      <div className="content">서울대 연구공원 웨딩홀</div>
+      <div className="info">서울대 연구공원 웨딩홀</div>
     </LazyDiv>
   )
 }
