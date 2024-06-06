ff92aa3a0ce3e4bd4a745586564854cc0dadd7b0
juhonamnam <jhnamnam@gmail.com>
2024-06-06 15:40:51 +0900

    remove attendance modal

---
+++ src/component/information/attendance.tsx
@@ -0,0 +1,319 @@
+import { BRIDE, GROOM, WEDDING_DATE } from "../../const"
+import { Button } from "../button"
+import { useModal } from "../store"
+import { useEffect, useRef, useState } from "react"
+import { ReactComponent as HeartIcon } from "../../image/heart-icon.svg"
+import { ReactComponent as CalendarIcon } from "../../image/calendar-icon.svg"
+import { ReactComponent as MarkerIcon } from "../../image/marker-icon.svg"
+
+const RULES = {
+  name: {
+    maxLength: 10,
+  },
+  count: {
+    min: 0,
+    default: 1,
+  },
+}
+
+export const AttendanceInfo = () => {
+  const { openModal, closeModal } = useModal()
+
+  const initialized = useRef(false)
+
+  useEffect(() => {
+    if (initialized.current) return
+    initialized.current = true
+    openModal({
+      className: "attendance-info-modal",
+      header: <div className="title">참석의사 전달 안내</div>,
+      content: (
+        <>
+          <div className="info-message">
+            축하의 마음으로 참석해주시는
+            <br />
+            모든 분들을 귀하게 모실 수 있도록
+            <br />
+            참석 및 식사 여부를 미리 여쭙고자 합니다.
+            <div className="break" />
+            부담없이 알려주시면
+            <br />
+            정성껏 준비하겠습니다.
+          </div>
+          <div className="wedding-info">
+            <HeartIcon /> 신랑 {GROOM} & 신부 {BRIDE}
+            <br />
+            <CalendarIcon /> {WEDDING_DATE.format("YYYY년 MMMM D일 dddd A h시")}
+            <br />
+            <MarkerIcon /> 서울대학교 연구공원 웨딩홀
+          </div>
+        </>
+      ),
+      footer: (
+        <>
+          <Button
+            buttonStyle="style2"
+            onClick={() => {
+              closeModal()
+              openModal(attendanceModalInfo)
+            }}
+          >
+            참석의사 전달하기
+          </Button>
+          <Button
+            buttonStyle="style2"
+            className="bg-light-grey-color text-dark-color"
+            onClick={closeModal}
+          >
+            닫기
+          </Button>
+        </>
+      ),
+    })
+  }, [openModal, closeModal])
+
+  return (
+    <div className="info-card">
+      <div className="label">참석의사</div>
+      <div className="content">
+        신랑, 신부에게 참석의사를
+        <br />
+        미리 전달할 수 있어요.
+      </div>
+
+      <div className="break" />
+
+      <Button
+        style={{ width: "100%" }}
+        onClick={() => {
+          openModal(attendanceModalInfo)
+        }}
+      >
+        참석의사 전달하기
+      </Button>
+    </div>
+  )
+}
+
+export const AttendanceModalContent = () => {
+  const { closeModal } = useModal()
+  const inputRef = useRef({ side: {}, meal: {} }) as React.MutableRefObject<{
+    side: {
+      groom: HTMLInputElement
+      bride: HTMLInputElement
+    }
+    name: HTMLInputElement
+    meal: {
+      yes: HTMLInputElement
+      undecided: HTMLInputElement
+      no: HTMLInputElement
+    }
+    count: HTMLInputElement
+  }>
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <form
+      id="attendance-form"
+      className="form"
+      onSubmit={async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+          const side = inputRef.current.side.groom.checked
+            ? "groom"
+            : inputRef.current.side.bride
+              ? "bride"
+              : null
+          const name = inputRef.current.name.value
+          const meal = inputRef.current.meal.yes.checked
+            ? "yes"
+            : inputRef.current.meal.undecided.checked
+              ? "undecided"
+              : inputRef.current.meal.no.checked
+                ? "no"
+                : null
+          const count = Number(inputRef.current.count.value)
+
+          if (!side) {
+            alert("신랑 또는 신부를 선택해주세요.")
+            return
+          }
+
+          if (!name) {
+            alert("성함을 입력해주세요.")
+            return
+          }
+          if (name.length > RULES.name.maxLength) {
+            alert(`성함을 ${RULES.name.maxLength}자 이하로 입력해주세요.`)
+            return
+          }
+
+          if (!meal) {
+            alert("식사 여부를 선택해주세요.")
+            return
+          }
+
+          if (isNaN(count)) {
+            alert("참석인원을 입력해주세요.")
+            return
+          }
+          if (count < RULES.count.min) {
+            alert(`참석인원을 ${RULES.count.min}명 이상으로 입력해주세요.`)
+            return
+          }
+
+          const res = await fetch(
+            `${process.env.REACT_APP_SERVER_URL}/attendance`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ side, name, meal, count }),
+            },
+          )
+          if (!res.ok) {
+            throw new Error(res.statusText)
+          }
+
+          alert("참석의사가 성공적으로 전달되었습니다.")
+          closeModal()
+        } catch {
+          alert("참석의사 전달에 실패했습니다.")
+        } finally {
+          setLoading(false)
+        }
+      }}
+    >
+      <div className="input-group">
+        <div className="label">구분</div>
+        <div className="select-input">
+          <label>
+            <input
+              disabled={loading}
+              type="radio"
+              name="side"
+              value="groom"
+              hidden
+              defaultChecked
+              ref={(ref) =>
+                (inputRef.current.side.groom = ref as HTMLInputElement)
+              }
+            />
+            <span>신랑</span>
+          </label>
+
+          <label>
+            <input
+              disabled={loading}
+              type="radio"
+              name="side"
+              value="bride"
+              hidden
+              ref={(ref) =>
+                (inputRef.current.side.bride = ref as HTMLInputElement)
+              }
+            />
+            <span>신부</span>
+          </label>
+        </div>
+      </div>
+
+      <div className="input-group">
+        <div className="label">성함</div>
+        <div className="input">
+          <input
+            disabled={loading}
+            type="text"
+            placeholder="참석자 성함을 입력해주세요."
+            maxLength={RULES.name.maxLength}
+            ref={(ref) => (inputRef.current.name = ref as HTMLInputElement)}
+          />
+        </div>
+      </div>
+
+      <div className="input-group">
+        <div className="label">식사</div>
+        <div className="radio-input">
+          <label>
+            <input
+              disabled={loading}
+              type="radio"
+              name="meal"
+              value="yes"
+              ref={(ref) =>
+                (inputRef.current.meal.yes = ref as HTMLInputElement)
+              }
+            />
+            <span>예정</span>
+          </label>
+
+          <label>
+            <input
+              disabled={loading}
+              type="radio"
+              name="meal"
+              value="undecided"
+              ref={(ref) =>
+                (inputRef.current.meal.undecided = ref as HTMLInputElement)
+              }
+            />
+            <span>미정</span>
+          </label>
+
+          <label>
+            <input
+              disabled={loading}
+              type="radio"
+              name="meal"
+              value="no"
+              ref={(ref) =>
+                (inputRef.current.meal.no = ref as HTMLInputElement)
+              }
+            />
+            <span>불참</span>
+          </label>
+        </div>
+      </div>
+
+      <div className="input-group">
+        <div className="label">참석인원 (본인 포함)</div>
+        <div>
+          <input
+            disabled={loading}
+            type="number"
+            min={RULES.count.min}
+            defaultValue={RULES.count.default}
+            ref={(ref) => (inputRef.current.count = ref as HTMLInputElement)}
+          />
+          명
+        </div>
+      </div>
+    </form>
+  )
+}
+export const AttendanceModalFooter = () => {
+  const { closeModal } = useModal()
+  return (
+    <>
+      <Button buttonStyle="style2" type="submit" form="attendance-form">
+        전달하기
+      </Button>
+      <Button
+        buttonStyle="style2"
+        className="bg-light-grey-color text-dark-color"
+        onClick={closeModal}
+      >
+        닫기
+      </Button>
+    </>
+  )
+}
+
+export const attendanceModalInfo = {
+  className: "attendance-modal",
+  header: <div className="title">참석의사 전달하기</div>,
+  content: <AttendanceModalContent />,
+  footer: <AttendanceModalFooter />,
+}
